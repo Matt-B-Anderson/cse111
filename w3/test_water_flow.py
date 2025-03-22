828aa12b3c5994d99cd9d2d00369b8b9c6f1e687
Matthew Anders <manderson0182@gmail.com>
2025-03-22 00:12:57 -0400

    updated water_flow

--- w3/test_water_flow.py
+++ w3/test_water_flow.py
@@ -23,7 +23,27 @@ def test_pressure_loss_from_pipe():
     assert water_flow.pressure_loss_from_pipe(0.286870, 1000.0, 0.013, 1.65) == approx(-61.576, abs=0.001)
     assert water_flow.pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, abs=0.001)
 
-
-# Call the main function that is part of pytest so that the
-# computer will execute the test functions in this file.
+def test_pressure_loss_from_fittings():
+    assert water_flow.pressure_loss_from_fittings(0.00, 3) == approx(0.00, abs=0.001)
+    assert water_flow.pressure_loss_from_fittings(1.65, 0) == approx(0.00, abs=0.001)
+    assert water_flow.pressure_loss_from_fittings(1.65, 2) == approx(-0.109, abs=0.001)
+    assert water_flow.pressure_loss_from_fittings(1.75, 2) == approx(-0.122, abs=0.001)
+    assert water_flow.pressure_loss_from_fittings(1.75, 5) == approx(-0.306, abs=0.001)
+    
+def test_reynolds_number():
+    assert water_flow.reynolds_number(0.048692, 0.00) == approx(0, abs=1)
+    assert water_flow.reynolds_number(0.048692, 1.65) == approx(80069, abs=1)
+    assert water_flow.reynolds_number(0.048692, 1.75) == approx(84922, abs=1)
+    assert water_flow.reynolds_number(0.286870, 1.65) == approx(471729, abs=1)
+    assert water_flow.reynolds_number(0.286870, 1.75) == approx(500318, abs=1)
+    
+def test_pressure_loss_from_pipe_reduction():
+    assert water_flow.pressure_loss_from_pipe_reduction(0.28687, 0.000003498, 0.048692) == approx(0.000, abs=0.001)
+    assert water_flow.pressure_loss_from_pipe_reduction(0.28687, 1.65, 0.048692) == approx(-163.744, abs=0.001)
+    assert water_flow.pressure_loss_from_pipe_reduction(0.28687, 1.75, 0.048692) == approx(-184.182, abs=0.001)
+    
+    
+    
+    
+    
 pytest.main(["-v", "--tb=line", "-rN", __file__])
